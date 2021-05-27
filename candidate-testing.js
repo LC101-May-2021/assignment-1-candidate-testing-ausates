0364f8af81acd42a983638cb2cdd7e90f996ee72
ausates <>
2021-05-27 23:16:52 +0000

    I finished everything... I hope.

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,12 +5,12 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question = "";
+let correctAnswer = "";
+let candidateAnswer = "";
+let questions = ['Who was the first American woman in space? ', "True or false: 5 kilometer == 5000 meters: ", "(5 + 3)/2 * 10 =? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the maximum crew size for the ISS (type a number): "];
+let correctAnswers = ["sally ride", "true", "40", "trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -20,42 +20,50 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  question = input.question('Who was the first American woman in space? ');
-  candidateAnswer = question;
-  candidateAnswer = candidateAnswer.toLowerCase();
-  // this part tests for spaces in the answer
-  spaceInAnswer = false;
-  if (candidateAnswer.indexOf(' ') >= 0) {
-    spaceInAnswer = true;
+
+  for (i=0; i < questions.length; i++) {
+    question = input.question(`${questions[i]}`);
+    candidateAnswer = question;
+    candidateAnswer = candidateAnswer.toLowerCase();
+    candidateAnswers.push(candidateAnswer)
+    correctAnswer = correctAnswers[i];
+    if (candidateAnswer === correctAnswer) {
+    console.log("Excellent, you are so right and so smart!");
+  } else if (candidateAnswer !== correctAnswer) {
+    console.log("Sad day... you were... wRoNg.");
+  };
+  console.log(candidateAnswers)
+
+    // // this part tests for spaces in the answer
+    // spaceInAnswer = false;
+    // if (candidateAnswer.indexOf(' ') >= 0) {
+    //   spaceInAnswer = true;
+    // }
+    // I originally capitalized answers for comparison
+    // Now, I just lower case everything.
+
+
+    // if (spaceInAnswer === true) {
+    //   words = candidateAnswer.split(" ");
+    //   firstAnswer = words[0].charAt(0).toUpperCase() + words[0].slice(1).toLowerCase();
+    //   lastAnswer = words[1].charAt(0).toUpperCase() + words[1].slice(1).toLowerCase();
+    //   candidateAnswer = firstAnswer + " " + lastAnswer;
+    // }
+    
   }
-  // this part capitalizes the first letter of each word
-  // it also lower cases the rest of each word
-  // this is to ensure improper capitalization doesn't cause
-  // an "incorrect answer".
-  if (spaceInAnswer === true) {
-    words = candidateAnswer.split(" ");
-    firstAnswer = words[0].charAt(0).toUpperCase() + words[0].slice(1).toLowerCase();
-    lastAnswer = words[1].charAt(0).toUpperCase() + words[1].slice(1).toLowerCase();
-    candidateAnswer = firstAnswer + " " + lastAnswer;
-  }
-  correctAnswer = "Sally Ride";
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-  let grade;
-
-  if (candidateAnswer === correctAnswer) {
-    console.log("Excellent, you are so right and so smart!")
-    grade = true;
-    return grade;
-  } else if (candidateAnswer !== correctAnswer) {
-    console.log("Sad day... you were... wRoNg.")
-    grade = false;
-    return grade;
+  let grade = 0;
+  for (let i = 0; i < correctAnswers.length; i++) {
+    if (candidateAnswers[i] === correctAnswers[i]){
+      grade++;
+    }
   }
+
+  console.log(`Your grade is ${grade} out of 5.`);
 
   return grade;
 }
@@ -63,14 +71,14 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
+  let spaceInName;
   if (candidateName.indexOf(' ') >= 0) {
     spaceInName = true;
   }
   // this part capitalizes the first letter of each word
   // it also lower cases the rest of each word
-  // this is to ensure improper capitalization doesn't cause
-  // an "incorrect answer".
-  if (spaceInName === true) {
+  // This is to properly capitalize the name.
+  if (spaceInName) {
     words = candidateName.split(" ");
     firstName = words[0].charAt(0).toUpperCase() + words[0].slice(1).toLowerCase();
     lastName = words[1].charAt(0).toUpperCase() + words[1].slice(1).toLowerCase();

25977d49c33621607fcccb9c1117f91849e38fbf
ausates <>
2021-06-04 01:09:12 +0000

    defined variables to reflect the crazy auto grader...

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,8 +5,8 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "";
-let correctAnswer = "";
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions = ['Who was the first American woman in space? ', "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
@@ -20,15 +20,6 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-  
-  //   if (candidateAnswer === correctAnswer) {
-  //   console.log("Excellent, you are so right and so smart!");
-  // } else if (candidateAnswer !== correctAnswer) {
-  //   console.log("Sad day... you were... wRoNg.");
-  // };
-
-    // // this part tests for spaces in the answer
 
 
   for (i=0; i < questions.length; i++) {

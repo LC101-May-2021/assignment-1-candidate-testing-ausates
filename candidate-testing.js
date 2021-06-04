5300d0dbc52a4c830173489a318e7439917d501d
ausates <>
2021-06-04 00:46:54 +0000

    I fixed the capitalization testing... as well as asking the questions exactly.

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,8 +8,8 @@ let candidateName = "";
 let question = "";
 let correctAnswer = "";
 let candidateAnswer = "";
-let questions = ['Who was the first American woman in space? ', "True or false: 5 kilometer == 5000 meters: ", "(5 + 3)/2 * 10 =? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the maximum crew size for the ISS (type a number): "];
-let correctAnswers = ["sally ride", "true", "40", "trajectory", "3"];
+let questions = ['Who was the first American woman in space? ', "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 =? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the maximum crew size for the ISS (type a number): "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
 
@@ -24,7 +24,7 @@ function askQuestion() {
   for (i=0; i < questions.length; i++) {
     question = input.question(`${questions[i]}`);
     candidateAnswer = question;
-    candidateAnswer = candidateAnswer.toLowerCase();
+    // candidateAnswer = candidateAnswer.toLowerCase();
     candidateAnswers.push(candidateAnswer)
     correctAnswer = correctAnswers[i];
   //   if (candidateAnswer === correctAnswer) {
